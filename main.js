0bb5a5ff18c461acd76570bf0640df1dddd1ceff
钢翼 <659240788@qq.com>
2022-02-11 14:38:08 +0800

    it1352未登录允许查看文章

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-(function () {
+﻿(function () {
   //允许复制
   let preElList = document.querySelectorAll('pre')
   preElList.forEach(el => {
     el.style.userSelect = 'auto'
     let codeEl = el.querySelector('code')
-    codeEl.style.userSelect = 'auto'
+    if(codeEl ){
+        codeEl.style.userSelect = 'auto'
+    }
   })
 
   function InjectJs(jsPath) {
@@ -14,4 +16,15 @@
     document.head.appendChild(s);
   }
   InjectJs("hack.js");
+
+  //移除it1352的登录限制
+  let maskDom = document.querySelector('.arc-body-main-more')
+  if(maskDom){
+    maskDom.style.display='none'
+  }
+  let codeDom = document.querySelector('.arc-body-main')
+  if(codeDom){
+    codeDom.style.height='auto'
+    codeDom.style.overflow='auto'
+  }
 })()
